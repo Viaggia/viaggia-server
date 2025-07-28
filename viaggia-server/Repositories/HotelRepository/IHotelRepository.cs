26d9457ce7f67f56ecec37106e6d771dc516aa13
PLuiza12 <pluiza64@gmail.com>
2025-07-28 19:01:31 -0300

    Ajuste dos repositories da entidade hotel

--- viaggia-server/Repositories/HotelRepository/IHotelRepository.cs
+++ viaggia-server/Repositories/HotelRepository/IHotelRepository.cs
@@ -1,6 +1,11 @@
 ﻿using viaggia_server.Models.Hotels;
 using viaggia_server.Models.Addresses;
 using viaggia_server.Models.HotelRoomTypes;
+using viaggia_server.Models.HotelDates;
+using viaggia_server.Models.Medias;
+using viaggia_server.Models.Reviews;
+using viaggia_server.Models.Packages;
+using viaggia_server.Models.Commodities;
 
 namespace viaggia_server.Repositories.HotelRepository
 {
@@ -21,6 +26,42 @@ namespace viaggia_server.Repositories.HotelRepository
         Task<HotelRoomType> AddRoomTypeAsync(HotelRoomType roomType);
         Task<HotelRoomType?> GetRoomTypeByIdAsync(int roomTypeId);
         Task<IEnumerable<HotelRoomType>> GetHotelRoomTypesAsync(int hotelId);
+
+        //Hotel Dates
+        Task<IEnumerable<HotelDate>> GetHotelDatesAsync(int hotelId);
+        Task<HotelDate?> GetHotelDateByIdAsync(int hotelDateId);
+        Task<HotelDate> AddHotelDateAsync(HotelDate hotelDate);
+
+        //Medias
+        Task<IEnumerable<Media>> GetMediasByHotelIdAsync(int hotelId);
+        Task<Media?> GetMediaByIdAsync(int mediaId);
+        Task<Media> AddMediaAsync(Media media);
+
+        //Reviews
+        Task<IEnumerable<Review>> GetReviewsByHotelIdAsync(int hotelId);
+        Task<Review?> GetReviewByIdAsync(int reviewId);
+        Task<Review> AddReviewAsync(Review review);
+
+        //Packages
+        Task<IEnumerable<Package>> GetPackagesByHotelIdAsync(int hotelId);
+        Task<Package?> GetPackageByIdAsync(int packageId);
+        Task<Package> AddPackageAsync(Package package);
+
+        //Commodities
+        Task<IEnumerable<Commoditie>> GetCommoditiesByHotelIdAsync(int hotelId);
+        Task<Commoditie?> GetCommoditieByIdAsync(int commodityId);
+        Task<Commoditie> AddCommoditieAsync(Commoditie commodity);
+
+        //CommoditieServices 
+        Task<IEnumerable<CommoditieServices>> GetCommoditieServicesByHotelIdAsync(int hotelId);
+        Task<CommoditieServices?> GetCommoditieServiceByIdAsync(int commodityServiceId);
+        Task<CommoditieServices> AddCommoditieServiceAsync(CommoditieServices commodityService);
+
+      
+
+
+
+
     }
 }
 
